b927d6faad9c0ace51fca3bded3ae167df6e7d1d
OreyM <oreymgt@gmail.com>
2019-05-02 00:18:47 +0300

    translate info 3

--- error/debug.php
+++ error/debug.php
@@ -5,7 +5,7 @@
             </div>
             <div class="error-content-hl">
                 <p class="p-hl">
-                    <span>Тип переменной: </span>
+                    <span>Value type: </span>
                     <strong>
                         <?= $debugInfo['varType'] ?>
                         <?php if ( isset( $debugInfo['addInfoType'] ) ): ?>
@@ -15,7 +15,7 @@
                 </p>
 
                 <?php if ( $debugInfo['varType'] === 'resource' ): ?>
-                    <p><span>Тип ресурса: </span><?= $debugInfo['var'] ?></p>
+                    <p><span>Resource type: </span><?= $debugInfo['var'] ?></p>
                 <?php elseif ( $debugInfo['varType'] === 'unknown type' && $debugInfo['var'] === FALSE ): ?>
                     <!-- NO INFO -->
                 <?php elseif ( $debugInfo['varType'] === 'array' || $debugInfo['varType'] === 'object' ): ?>
@@ -23,12 +23,12 @@
                             <?= $debugData ?>
                         <?php endforeach; ?>
                 <?php else: ?>
-                    <p><span>Значение: </span><strong><?= $debugInfo['var'] ?></strong></p>
+                    <p><span>Value: </span><strong><?= $debugInfo['var'] ?></strong></p>
                 <?php endif; ?>
             </div>
             <div class="debug-footer-hl">
                 <span>
-                    # Дебаг вызван: <?= $backtrace[0]['file']; ?> || строка: <?= $backtrace[0]['line']; ?>
+                    # Debug called: <?= $backtrace[0]['file']; ?> || line: <?= $backtrace[0]['line']; ?>
                 </span>
             </div>
         </div>
--- error/devError.php
+++ error/devError.php
@@ -3,8 +3,8 @@
             <div class="error-header-hl"><?= $errType; ?> [code:<?= $errno; ?>]</div>
             <div class="error-content-hl">
                 <p class="error-text"><?= $errstr; ?></p>
-                <p><strong>Файл:</strong> <?= $errfile; ?></p>
-                <p><strong>Строка:</strong> <?= $errline; ?></p>
+                <p><strong>File:</strong> <?= $errfile; ?></p>
+                <p><strong>Line:</strong> <?= $errline; ?></p>
             </div>
         </div>
 </div>
